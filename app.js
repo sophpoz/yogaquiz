3d32f8fb4d56f88f73c61f5c3e156e67c10579c2
Sophia Pozzi <sopozzi@gmail.com>
2016-08-03 19:59:53 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,55 @@
+// ARRAY OF OBJECTS: [{}]
+
+var questionModel = {
+  question:'Yoga is a religion',
+  answers:['True', 'False'],
+  correctAnswer: 'False',
+  incorrectFeedback:'False! Yoga is not a religion',
+  correctFeedback: 'Correct! Yoga is not a religion'
+};
+
+var question2 = {
+	question: 'You have to be flexible to do yoga',
+	answers:['True', 'False'],
+	correctAnswer:'False',
+	incorrectFeedback: 'False! Thats like saying youre too wear to go to the gym',
+	correctFeedback: 'Correct! Youre a yogi already'
+};
+
+var question3 = {
+	question: 'What are the benefits of yoga?',
+	answers: ['Its a great workout', 'It helps calm the mind', 'Both!'],
+	correctAnswer: 'Both!',
+	incorrectFeedback: 'False, youre almost there...',
+	correctFeedback: 'Correct! Yoga has mind and body benefits'
+};
+
+var question4 = {
+	question: 'What does Namaste mean?',
+	answers: ['I bow to you', 'Thank you', 'Peace'],
+	correctAnswer: 'I bow to you',
+	incorrectFeedback: 'False! Translated directly, Namaste means I bow to you, its a sign of great respect',
+	correctFeedback: 'Correct! Translated directly, Namaste means I bow to you, its a sign of great respect'
+};
+
+var question5 = {
+	question: 'You have to learn Sanskrit to follow along in class',
+	answers: ['True', 'False'],
+	correctAnswer: 'False',
+	incorrectFeedback: 'False! Your instructor will lead the class by calling the poses in both English and Sanskrit',
+	correctFeedback: 'Correct! Your instructor will lead the class by calling the poses in both English and Sanskrit'
+};
+
+$('.play_again').click(resetQuiz);
+
+function resetQuiz(){
+
+}
+
+$('#get_started').click()
+
+// jQuery
+// Score
+// Reset button
+// Disabling function
+// Next function
