729b984c06624b9619e92c5a7e9a53c8175c9c8a
Sophia Pozzi <sopozzi@gmail.com>
2016-08-13 19:37:17 -0700

    dynamic

--- app.js
+++ app.js
@@ -1,52 +1,117 @@
-// ARRAY OF OBJECTS: [{}]
-
-var questionModel = {
-  question:'Yoga is a religion',
+var questions = [{
+  question:'1. Yoga is a religion',
   answers:['True', 'False'],
   correctAnswer: 'False',
   incorrectFeedback:'False! Yoga is not a religion',
   correctFeedback: 'Correct! Yoga is not a religion'
-};
-
-var question2 = {
-	question: 'You have to be flexible to do yoga',
+}, 
+{
+	question: '2. You have to be flexible to do yoga',
 	answers:['True', 'False'],
 	correctAnswer:'False',
-	incorrectFeedback: 'False! Thats like saying youre too wear to go to the gym',
+	incorrectFeedback: 'False! Thats like saying youre too weak to go to the gym',
 	correctFeedback: 'Correct! Youre a yogi already'
-};
-
-var question3 = {
-	question: 'What are the benefits of yoga?',
+},
+{
+	question: '3. What are the benefits of yoga?',
 	answers: ['Its a great workout', 'It helps calm the mind', 'Both!'],
 	correctAnswer: 'Both!',
 	incorrectFeedback: 'False, youre almost there...',
 	correctFeedback: 'Correct! Yoga has mind and body benefits'
-};
-
-var question4 = {
-	question: 'What does Namaste mean?',
+},
+{
+	question: '4. What does Namaste mean?',
 	answers: ['I bow to you', 'Thank you', 'Peace'],
 	correctAnswer: 'I bow to you',
 	incorrectFeedback: 'False! Translated directly, Namaste means I bow to you, its a sign of great respect',
 	correctFeedback: 'Correct! Translated directly, Namaste means I bow to you, its a sign of great respect'
-};
-
-var question5 = {
-	question: 'You have to learn Sanskrit to follow along in class',
+},
+{
+	question: '5. You have to learn Sanskrit to follow along in class',
 	answers: ['True', 'False'],
 	correctAnswer: 'False',
 	incorrectFeedback: 'False! Your instructor will lead the class by calling the poses in both English and Sanskrit',
 	correctFeedback: 'Correct! Your instructor will lead the class by calling the poses in both English and Sanskrit'
-};
+}];
 
-$('.play_again').click(resetQuiz);
 
-function resetQuiz(){
 
-}
+$(document).ready(function(){
+	$('#question').hide();
+	$('#final-area').hide();
+	
+	//some to say 'hey we're at index 2' placeholder variable
+	var currentQuestionIndex = 0;
+	var currentQuestion = questions[currentQuestionIndex];
+	var count = 0;
+	
+	$('#get_started').click(function(){
+		$('#question').show();
+		loadquestion(currentQuestion);
+		$('.start-area').hide();
+		// hide next button
+		$('#next').hide();
+	});
+	// click event on the answer to give feedback
+	$('#answers').click(function(event){
+	  // console.log(event.target)
+	  // $(event.target)
+	  // check the answer
+	  if ( $(event.target).text() === currentQuestion.correctAnswer){
+	    // show feedback
+	    $('#feedback').text(currentQuestion.correctFeedback);
+	    count +=1
+	    $('.count').text(count);
+	  } else {
+	    // show feedback
+	    $('#feedback').text(currentQuestion.incorrectFeedback);
+	  }
+	  // set next question variables
+	  currentQuestionIndex +=1; 
+	  currentQuestion = questions[currentQuestionIndex];
+	  // show next button
+	  // $('#answers').attr("disabled", true);
+	  $('#next').show();
+	})
 
-$('#get_started').click()
+	//add a click event to #next button to increment element in question array
+		$('#next').click(function(){
+		if (currentQuestionIndex < questions.length){
+		  loadquestion(currentQuestion);	
+		} else { 
+		  $('#question').hide();
+		  $('#final-area').show();
+		  
+		}
+	});
+
+	//play again
+	$('.playagain').click(function(){
+		count = 0;
+		currentQuestionIndex =0; 
+	  	currentQuestion = questions[currentQuestionIndex];
+		$('#final-area').hide();
+		$('.start-area').show();
+	})
+
+  function loadquestion(question){
+		//question argument
+		// update h2
+		$('h2').text(question.question);
+		// update #answers
+		$('#answers').text('');
+	  for (i = 0; i < question.answers.length; i++){
+			$('#answers').append('<li>' + question.answers[i]  +  '</li> ');
+		}
+		$('#answers').attr("disabled", true);
+		// update #feedback to ''
+		$('#feedback').text('');
+		$('#next').hide();
+		
+	}
+
+})
+
 
 // jQuery
 // Score
